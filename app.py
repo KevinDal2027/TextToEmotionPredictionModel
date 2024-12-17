0b9dcee2b9372bcda848045765b1aa74757cad9f
Kevin Nguyen <kv375322@dal.ca>
2024-12-16 21:36:02 -0400

    Finished setting up app to deploy on local

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, render_template, redirect, url_for
+import matplotlib.pyplot as plt
+import io
+import base64
+from predictionModel import model
+from predictionModel import wordsArray
+from predictionModel import emotionArray
+import spacy
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    test = request.form['user_text']
+    testIndices = []
+    test = test.lower()
+
+    nlp = spacy.load('en_core_web_sm')
+    test = nlp(test)
+
+    for word in test:
+        if word.text in list(wordsArray) and word.pos_ in ['VERB', "ADJ", "ADV", "NOUN"]:
+            if word.pos_ == 'ADJ':
+                testIndices.append(list(wordsArray).index(word.lemma_))
+            testIndices.append(list(wordsArray).index(word.lemma_))
+    testDF = pd.DataFrame({'Word': testIndices})
+
+    if len(testIndices) <= 0:
+        return render_template('result.html', labels=emotionArray, data=[0,0,0,0,0,0])
+
+    emotionScalar = [0] * len(emotionArray)
+
+    for i in model.predict(testDF):
+        emotionScalar[i] += 1
+
+    finalEmotionPieScalar = []
+    finalEmotionPie = []
+    for i in emotionScalar:
+        if i > 0 and emotionArray[emotionScalar.index(i)] not in finalEmotionPie:
+            finalEmotionPie.append(emotionArray[emotionScalar.index(i)])
+            finalEmotionPieScalar.append(i)
+
+    total = sum(finalEmotionPieScalar)
+    finalEmotionPieScalarPercentage = [(count / total) * 100 for count in finalEmotionPieScalar]
+
+    return render_template('result.html', labels=finalEmotionPie, data=finalEmotionPieScalarPercentage)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ predictionModel.py
@@ -0,0 +1,26 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+
+#LOAD DataFrame
+wordsInDF = pd.read_csv("static/Extracted_Emotions_VERB_ADJ_ADV.csv")
+
+wordsArray = wordsInDF['Word'].unique()
+
+emotionArray = ['sadness', 'anger', 'love', 'surprise', 'fear', 'happy']
+
+# LOADING FROM Emotion_Encoded
+wordsInDF = pd.read_csv("static/Encoded_VERB_ADJ_ADV.csv")
+
+wordsInDF = wordsInDF.drop('Unnamed: 0', axis=1)
+
+# USING DECISION TREE TO MAKE PREDICTION
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier()
+#TRAIN
+model.fit(wordsInDF.drop("Emotion", axis = 1), wordsInDF.drop("Word", axis = 1))
